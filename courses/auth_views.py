f1f06a1b782d6db4a2827c7fc3414bf26c3b0fdd
Yousef Hany Mohamed <yousefhani537@gmail.com>
2025-03-15 05:13:22 +0200

    done

--- courses/auth_views.py
+++ courses/auth_views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login, authenticate
 from django.contrib import messages
+from .forms import UserCreationForm
+from .models import UserProfile
 
 def register(request):
     if request.method == 'POST':
